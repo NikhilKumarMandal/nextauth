64acfdf1445ed39b2c9d836f96d8ef948629545d
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-03-28 21:27:40 +0530

    Add bcryptjs and nodemailer dependencies
    The following changes were made:
    
    1. New dependencies added to package.json:
    - bcryptjs: ^2.4.3
    - nodemailer: ^6.9.13
    2. New devDependencies added to package.json:
    - @types/bcryptjs: ^2.4.6
    - @types/nodemailer: ^6.4.14
    3. The file package-lock.json was updated to reflect the new dependencies and their versions.
    4. The file node\_modules was created, containing the new dependencies and their dependencies.
    5. The file src/app/dbConfig/dbConfig.ts was deleted.
    6. The file src/app/models/UserModel.js was deleted.
    
    Note: The diff does not show the removal of the dbConfig and UserModel files, but they were deleted in the same commit as the addition of the new dependencies.

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,49 @@
+import User from '@/models/userModel';
+import nodemailer from 'nodemailer'
+import bcryptjs from 'bcryptjs'
+
+export const sendEmail = async({email,emailType,userId}:any) => {
+    try {
+      const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+
+      if (emailType === 'VERIFY') {
+        await User.findByIdAndUpdate(
+          userId,
+          {verifyToken : hashedToken,
+          verifyTokenExpiry: Date.now() + 3600000
+          })
+      } else if (emailType === 'RESET'){
+        await User.findByIdAndUpdate(
+          userId,
+          {forgetPasswordToken : hashedToken,
+          forgetPasswordTokenExpiry: Date.now() + 3600000
+          })
+      }
+
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false,
+            auth: {
+              user: "36b7a16aef06b7",
+              pass: "90ee4700055c07"
+            },
+          });
+
+          const mailOptions = {
+            from: "nikhilkumarmandal946@gmail.com",
+            to: email,
+            subject: emailType === 'VERIFY' ? 'verify your email' : "Reset your password", 
+            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            </p>`,
+          }
+
+    const mailResponse = await transporter.sendMail(mailOptions)
+    return mailResponse      
+    } catch (error:any) {
+        throw new Error(error.message)
+    }
+} 
+
+
